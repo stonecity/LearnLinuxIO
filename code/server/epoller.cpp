fdaa1a9ab9f0903c7935d0bca7f3ee9d06d5adb7
redstone <bluecart@live.com>
2023-02-19 21:08:37 +0800

    Tidy name

--- code/server/epoller.cpp
+++ code/server/epoller.cpp
@@ -6,46 +6,57 @@
 
 #include "epoller.h"
 
-Epoller::Epoller(int maxEvent):epollFd(epoll_create(512)), events(maxEvent){
+Epoller::Epoller(int maxEvent) : epollFd(epoll_create(512)), events(maxEvent)
+{
     assert(epollFd >= 0 && events.size() > 0);
 }
 
-Epoller::~Epoller() {
+Epoller::~Epoller()
+{
     close(epollFd);
 }
 
-bool Epoller::AddFd(int fd, uint32_t events) {
-    if(fd < 0) return false;
+bool Epoller::AddFd(int fd, uint32_t events)
+{
+    if (fd < 0)
+        return false;
     epoll_event ev = {0};
     ev.data.fd = fd;
     ev.events = events;
     return 0 == epoll_ctl(epollFd, EPOLL_CTL_ADD, fd, &ev);
 }
 
-bool Epoller::ModifyFd(int fd, uint32_t events) {
-    if(fd < 0) return false;
+bool Epoller::ModifyFd(int fd, uint32_t events)
+{
+    if (fd < 0)
+        return false;
     epoll_event ev = {0};
     ev.data.fd = fd;
     ev.events = events;
     return 0 == epoll_ctl(epollFd, EPOLL_CTL_MOD, fd, &ev);
 }
 
-bool Epoller::DeleteFd(int fd) {
-    if(fd < 0) return false;
+bool Epoller::DeleteFd(int fd)
+{
+    if (fd < 0)
+        return false;
     epoll_event ev = {0};
     return 0 == epoll_ctl(epollFd, EPOLL_CTL_DEL, fd, &ev);
 }
 
-int Epoller::Wait(int timeoutMs) {
+int Epoller::Wait(int timeoutMs)
+{
     return epoll_wait(epollFd, &events[0], static_cast<int>(events.size()), timeoutMs);
 }
 
-int Epoller::GetEventFd(size_t i) const {
+int Epoller::GetEventFd(size_t i) const
+{
     assert(i < events.size() && i >= 0);
     return events[i].data.fd;
 }
 
-uint32_t Epoller::GetEvents(size_t i) const {
+uint32_t Epoller::GetEvents(size_t i) const
+{
     assert(i < events.size() && i >= 0);
     return events[i].events;
 }
